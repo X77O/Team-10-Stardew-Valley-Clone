0f3c649cc09a6380ba1c2cfb7dde5a604367e6a2
X77O <74770751+X77O@users.noreply.github.com>
2022-11-29 13:41:08 +0100

    Crops, Tools, Animals Properties
    Crops, Tools, Animals Properties

--- entities.h
+++ entities.h
@@ -14,29 +14,65 @@ class Player : Entity
 public:
     Vector2 size = {};
     Vector2 position = {};
-    Color color = BLUE;
+    Color color = WHITE;
 
 };
 
 class Cow : Entity
 {
-
+    bool milked = false;
 };
 
 class Sheep : Entity
 {
-
+    bool sheared = false;
 };
 
 class Chicken : Entity
 {
-
+    bool laid_egg = false;
 };
+
+class Shovel : Entity
+{
+    bool is_in_player_inventory = false;
+};
+
+class WateringCan : Entity
+{
+    bool is_in_player_inventory = false;
+    int water_level = 0;
+};
+
+class CornCrop : Entity
+{
+    enum class Growth_Stage
+    {
+        EMPTY,
+        STAGE_1,
+        STAGE_2,
+        STAGE_3,
+        READY
+    };
+
+    bool is_harvestable = false;
+};
+
+class WheatCrop : Entity
+{
+    enum class Growth_Stage
+    {
+        EMPTY,
+        STAGE_1,
+        STAGE_2,
+        STAGE_3,
+        READY
+    };
+
+    bool is_harvestable = false;
+};
+
 
 void InitialisePlayer();
 void UpdatePlayer();
 void DrawPlayer();
-
-
-
-
