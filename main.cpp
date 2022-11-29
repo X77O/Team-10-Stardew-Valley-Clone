e41777215f7494d426708ecf88d6645f2c132bec
X77O <74770751+X77O@users.noreply.github.com>
2022-11-29 12:29:11 +0100

    With Franek and Igor's code
    Implemented all changes into this

--- main.cpp
+++ main.cpp
@@ -1,77 +1,42 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute raylib_compile_execute script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2022 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+
 #include <iostream>
 #include <stack>
 #include "raylib.h"
+#include "level.h"
+#include "../entities.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+void RenderLevel();
+
 int main(void)
 {
     // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1920;
-    const int screenHeight = 1080;
-
-    InitWindow(screenWidth, screenHeight, "Game");
-
-    Vector2 center = {(GetScreenWidth()), GetScreenHeight()};
-
-    float outerRadius = 180.f;
-    float startAngle = 180.f;
-    float endAngle = 270.f;
 
 
+    InitWindow(1280, 720, "Game");
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
+    InitialisePlayer();
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+
+    while (!WindowShouldClose()) 
     {
         // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
+        UpdatePlayer();
+        
 
         // Draw
-        //----------------------------------------------------------------------------------
+       
         BeginDrawing();
 
-        ClearBackground(DARKGREEN);
-        SetWindowTitle("Farm it Up!");
+        RenderLevel();
+        RenderTrees();
+        Counters();
+        Timer();
 
-        DrawCircleSector(center, outerRadius, startAngle, endAngle, 0, Fade(SKYBLUE, 0.3f));            // Pond
-        DrawCircleSectorLines(center, outerRadius, startAngle, endAngle, 0, BLACK);                     // Pond Outline
-
-        DrawRectangleGradientH(screenWidth * 0.f, screenHeight * 0.80f, 170, 180, DARKBROWN, BROWN);    // Shop
-        DrawRectangleLines(screenWidth * 0.f, screenHeight * 0.80f, 169, 181, BLACK);                   // Shop Outline
-        DrawText("S H O P !", screenWidth * 0.01f, screenHeight * 0.82f, 30, GOLD);                     // Shop Text
-
-        DrawRectangleGradientH(screenWidth * 0.75f, screenHeight * 0.05f, 420, 720, ORANGE, BEIGE);     // Farmland
+        DrawPlayer();
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
+
     }
 
     // De-Initialization
