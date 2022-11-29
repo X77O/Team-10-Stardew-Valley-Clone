d242c99b90e0044132891b48e7242f8d93e2afcb
TheGiraffeInTheBathtub <118812256+TheGiraffeInTheBathtub@users.noreply.github.com>
2022-11-29 21:19:55 +0100

    Update entities.cpp

--- entities.cpp
+++ entities.cpp
@@ -18,6 +18,9 @@ void InitialisePlayer()
 
 void UpdatePlayer()
 {
+    int width = GetScreenWidth();
+    int height = GetScreenHeight();
+
     Vector2 direction = { 0.f, 0.f };
 
     if (IsKeyDown(KEY_UP))
@@ -37,8 +40,39 @@ void UpdatePlayer()
         direction.x -= 1;
     }
 
+
+    if (player.position.x <= 0)
+    {
+        if (IsKeyDown(KEY_LEFT))
+        {
+            direction.x = 0;
+        }
+    }
+    if (player.position.x >= width -  player.size.x)
+    {
+        if (IsKeyDown(KEY_RIGHT))
+        {
+            direction.x = 0;
+        }
+    }
+    if (player.position.y <= 0)
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            direction.y = 0;
+        }
+    }
+    if (player.position.y >= height - player.size.y)
+    {
+        if (IsKeyDown(KEY_DOWN))
+        {
+            direction.y = 0;
+        }
+    }
+
     player.position.x += direction.x * PLAYER_SPEED;
     player.position.y += direction.y * PLAYER_SPEED;
+
 }
 
 void DrawPlayer()
